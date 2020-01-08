e4c697940428d2d8a626311413d90fb3f8c29437
Dawid Jonczak <djonczak1@gmail.com>
2020-01-08 18:34:51 +0100

    Character selection for Arena mode

---
+++ Assets/Scripts/MainMenu/CharacterSelection.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelection : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> characterList = new List<GameObject>();
+    private int index = 0;
+    public GameObject message;
+
+    void Start()
+    {
+        foreach(Transform child in transform)
+        {
+            characterList.Add(child.gameObject);
+        }
+
+        characterList[index].SetActive(true);
+    }
+
+    public void IndexDown()
+    {
+        characterList[index].gameObject.SetActive(false);
+        index--;
+        if (index < 0)
+        {
+            index = characterList.Count - 1;
+        }
+        characterList[index].gameObject.SetActive(true);
+    }
+
+    public void IndexUp()
+    {
+        characterList[index].gameObject.SetActive(false);
+        index++;
+        if (index == characterList.Count)
+        {
+            index = 0;
+        }
+        characterList[index].gameObject.SetActive(true);
+    }
+
+    public void SelectCharacter()
+    {
+        var character = characterList[index].GetComponent<CharacterSlot>();
+        if (character.isUnlocked == true)
+        {
+            message.GetComponent<Animator>().SetTrigger("Show");
+            message.GetComponentInChildren<UnityEngine.UI.Text>().text = "You have chosen " + character.characterName + ".";
+        }
+        else
+        {
+            message.GetComponent<Animator>().SetTrigger("Show");
+            message.GetComponentInChildren<UnityEngine.UI.Text>().text = "This character is locked !";
+        }
+    }
+
+}
---
+++ Assets/Scripts/MainMenu/CharacterSlot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSlot : MonoBehaviour
+{
+    public string characterName = "name";
+    public RuntimeAnimatorController controllerData;
+    public GameObject unlockInfo;
+    [SerializeField] private Color normalColor = new Color(255,255,255,255);
+
+    public bool isUnlocked;
+
+    public void Start()
+    {
+        if(isUnlocked == true)
+        {
+            unlockInfo.SetActive(false);
+            GetComponent<UnityEngine.UI.Image>().color = normalColor;
+        }
+    }
+}
